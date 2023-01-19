cad012f1983e73f81fcaec37a087162e1966c915
jbbisch <108563925+jbbisch@users.noreply.github.com>
2023-01-18 19:48:41 -0600

    Update server.py
    best i can do on this for now

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,4 @@
+import machinetranslation
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
@@ -5,16 +6,16 @@ import json
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def english_to_french(english_text):
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    french_text = TranslationEn.translate(english_text) # Write your code here
+    return "Translated text to French" french_text
 
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
+@app.route("/french_to_english")
+def french_to_english(french_text):
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    english_text = TranslationFr.translate(french_text) # Write your code here
+    return "Translated text to English" eglish_text
 
 @app.route("/")
 def renderIndexPage():
